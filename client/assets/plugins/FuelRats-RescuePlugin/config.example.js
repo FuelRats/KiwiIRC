945449aca4d21586d92af77f9a0a47af22ae8e0b
NoLifeKing <webmaster@itssimple.se>
2016-09-04 23:02:25 +0200

    Added code to build login form depending on settings

--- client/assets/plugins/FuelRats-RescuePlugin/config.example.js
+++ client/assets/plugins/FuelRats-RescuePlugin/config.example.js
@@ -1,5 +1,6 @@
 var rescuePlugin = {
 	AnnouncerUrl: '',
+	UseClientForm: true,
 	CommanderInfo: {
 		CMDRName: null,
 		IRCNick: null,
@@ -17,10 +18,10 @@ var rescuePlugin = {
 		Epic: null,
 		Open: null,
 		Notes: null,
-		Platform: null
+		Platform: null,
 		Successful: null,
 		System: null,
-		Title: nul,
+		Title: null,
 		CreatedAt: null,
 		UpdatedAt: null,
 		Rats: [],
@@ -28,6 +29,22 @@ var rescuePlugin = {
 		FirstLimpet: null
 	},
 	SetCommanderInfo: function() {
+        	function sanitizeCMDRName(cmdrName) {
+                	cmdrName = cmdrName.replace(/^cmdr/i,'').trim();
+	                return cmdrName;
+        	}
+
+	        function sanitizeIRCName(cmdrName) {
+        	        cmdrName = cmdrName.trim().replace(/^cmdr/i,'').trim();
+                	cmdrName = cmdrName.replace(/\s+/g,'_');
+	                cmdrName = cmdrName.split(' ').join('_');
+	                cmdrName = cmdrName.replace(/\./g, '');
+	                cmdrName = removeDiacritics(cmdrName);
+	                cmdrName = cmdrName.replace(/([^A-Za-z0-9\[\]{}\^´`_\\\|-]+)/g,'');
+	                cmdrName = cmdrName.replace(/^\d+/,'');
+	                return cmdrName;
+	        }
+
 		var cmdrName = document.getElementById('server_select_nick').value;
 		rescuePlugin.CommanderInfo.CMDRName = sanitizeCMDRName(cmdrName);
 		rescuePlugin.CommanderInfo.IRCNick = sanitizeIRCName(cmdrName);
@@ -52,6 +69,8 @@ var rescuePlugin = {
 		jQuery.ajax({
 			url: rescuePlugin.AnnouncerUrl,
 			type: 'GET',
+			crossDomain: true,
+			dataType: 'jsonp',
 			data: {
 				cmdrname: rescuePlugin.CommanderInfo.CMDRName,
 				EO2: rescuePlugin.CommanderInfo.EO2,
@@ -61,5 +80,42 @@ var rescuePlugin = {
 			},
 			success: function() { }
 		});
+	},
+	BuildLoginForm: function() {
+		if(rescuePlugin.UseClientForm) {
+			var cmdrLabel = jQuery('label[for="server_select_nick"]');
+			if(cmdrLabel.length == 0) { setTimeout(rescuePlugin.BuildLoginForm, 50); return; }
+			cmdrLabel.text('CMDR Name');
+
+			jQuery('.status').text('Please enter your details below...');
+
+			var systemItem = jQuery('.system');
+			if(systemItem.length == 0) {
+				systemItem = jQuery('<tr class="system"><td><label for="server_select_system">System name</label></td><td><input type="text" id="system" /></td></tr>');
+				cmdrLabel.parent().parent().after(systemItem);
+			}
+
+			var platformItem = jQuery('.platform');
+			if(platformItem.length == 0) {
+				platformItem = jQuery('<tr class="platform"><td><label for="server_select_platform">Platform</label></td><td><select name="platform" id="platform"><option value="PC">PC</option><option value="XB">XB</option></select></td></tr>');
+				systemItem.after(platformItem);
+			}
+
+			var o2Item = jQuery('.o2');
+			if(o2Item.length == 0) {
+				o2Item = jQuery('<tr class="o2"><td><label for="server_select_o2">Are you on emergency O2?</label></td><td><label style="font-size: .9em;"><input type="checkbox" id="EO2" style="width: auto;" value="NOT OK" /> YES! I have a timer in upper right corner.</td></tr>');
+				systemItem.after(o2Item);
+			}
+		} else {
+		}
+		jQuery('.have_pass').hide();
+		jQuery('.channel').hide();
 	}
 };
+
+jQuery(document).ready(function() {
+	rescuePlugin.BuildLoginForm();
+	jQuery('#kiwi .server_select button:contains("Start")').on('click', rescuePlugin.SetCommanderInfo);
+	var network = kiwi.components.Network();
+	network.on('connect', rescuePlugin.SendAnnounceToIRC);
+});
